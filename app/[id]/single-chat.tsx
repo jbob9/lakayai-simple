ba0a7c2a3234f73b4f7dd6fd21836b7edd94a3cd
jbob9 <berlenskyoliver99@gmail.com>
2025-11-20 12:57:10 -0500

    update the single chat page 2

--- app/[id]/single-chat.tsx
+++ app/[id]/single-chat.tsx
@@ -3,17 +3,24 @@
 import { Chat } from "@/components/chat";
 import { db } from "@/db";
 import { convertToUIMessages } from "@/lib/utils";
-import { useLiveQuery, useDocument } from "dexie-react-hooks";
+import { use, useState } from "react";
 
 const SingleChat = ({ chatId }: { chatId: string }) => {
-  const messages = useLiveQuery(async () => {
-    return await db.messages.where("chatId").equals(chatId).toArray();
-  });
+  const [loadedChat, setLoadedChat] = useState(false);
+  const getMessages = async () => {
+    const messages = await db.messages.where("chatId").equals(chatId).toArray();
+    setLoadedChat(true);
+    return messages;
+  };
+  const messages = use(getMessages());
 
   const Uimessages = convertToUIMessages(messages ?? []);
-
-  console.log(messages, 'messages')
-  console.log(Uimessages, 'Uimessages')
+  
+  if (!loadedChat) {
+    return <div>Loading...</div>;
+  }
+  console.log(messages, "messages");
+  console.log(Uimessages, "Uimessages");
 
   return <Chat id={chatId} initialMessages={Uimessages} />;
 };
