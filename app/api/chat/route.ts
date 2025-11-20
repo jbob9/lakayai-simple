594a50ee7ace3517ac535bb3d1879bfad59a6a83
jbob9 <berlenskyoliver99@gmail.com>
2025-11-20 11:47:39 -0500

    update chat api route

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,22 +1,51 @@
 import { myProvider } from "@/lib/ai/provider";
-import { streamText } from "ai";
+import { ChatSDKError } from "@/lib/errors";
+import { convertToUIMessages } from "@/lib/utils";
+import {
+  convertToModelMessages,
+  createUIMessageStream,
+  JsonToSseTransformStream,
+  smoothStream,
+  stepCountIs,
+  streamText,
+} from "ai";
 
 export async function POST(request: Request) {
-  const { id, messages } = await request.json();
+  const { id, messages, message } = await request.json();
+  const uiMessages = [...convertToUIMessages(messages), message];
 
-  const result = streamText({
-    model: myProvider.languageModel("chat-model"),
-    system:
-      "you are a friendly assistant! keep your responses concise and helpful.",
-    messages,
-    // onFinish: async ({ text }) => {
-    //   await createMessage({
-    //     id,
-    //     messages: [...messages, { role: "assistant", content: text }],
-    //     author: session.user?.email!,
-    //   });
-    // },
-  });
+  try {
+    const stream = createUIMessageStream({
+      execute: ({ writer: dataStream }) => {
+        const result = streamText({
+          model: myProvider.languageModel("chat-model"),
+          system:
+            "you are a friendly assistant! keep your responses concise and helpful.",
+          messages: convertToModelMessages(uiMessages),
+          stopWhen: stepCountIs(5),
+          experimental_transform: smoothStream({ chunking: "word" }),
+        });
 
-  return result.toTextStreamResponse({});
+        result.consumeStream();
+
+        dataStream.merge(
+          result.toUIMessageStream({
+            sendReasoning: true,
+          })
+        );
+      },
+      onError: () => {
+        return "Oops, an error occurred!";
+      },
+    });
+
+    return new Response(stream.pipeThrough(new JsonToSseTransformStream()));
+  } catch (error) {
+    console.log("Error in chat route:", error);
+    if (error instanceof ChatSDKError) {
+      return error.toResponse();
+    }
+
+    return new ChatSDKError("offline:chat").toResponse();
+  }
 }
--- components/chat.tsx
+++ components/chat.tsx
@@ -16,7 +16,7 @@ import {
   XIcon,
 } from "lucide-react";
 import { nanoid } from "nanoid";
-import { useCallback, useState } from "react";
+import { FormEvent, useCallback, useState } from "react";
 
 const suggestedActions = [
   {
@@ -70,24 +70,30 @@ export function Chat({
   const [messagesContainerRef, messagesEndRef] =
     useScrollToBottom<HTMLDivElement>();
 
-  const submitForm = useCallback(async () => {
-    window.history.replaceState({}, "", `/${id}`);
-    if (messages.length <= 0) {
-      await createChat({ message: input });
-    }
+  const submitForm = useCallback(
+    async (e: FormEvent<HTMLFormElement>) => {
+      console.log("submitForm");
+      e.preventDefault();
+      window.history.replaceState({}, "", `/${id}`);
+      console.log(input, "input");
+      if (messages.length <= 0) {
+        await createChat({ message: input });
+      }
 
-    sendMessage({
-      role: "user",
-      parts: [
-        {
-          type: "text",
-          text: input,
-        },
-      ],
-    });
+      sendMessage({
+        role: "user",
+        parts: [
+          {
+            type: "text",
+            text: input,
+          },
+        ],
+      });
 
-    setInput("");
-  }, [id, input, setInput, sendMessage]);
+      setInput("");
+    },
+    [id, input, setInput, sendMessage]
+  );
 
   let Icon = <SendIcon className="size-4" />;
 
@@ -142,7 +148,10 @@ export function Chat({
           </div>
         )}
 
-        <div className="flex flex-row gap-2 relative items-center w-full md:max-w-[500px] max-w-[calc(100dvw-32px) px-4 md:px-0">
+        <form
+          className="flex flex-row gap-2 relative items-center w-full md:max-w-[500px] max-w-[calc(100dvw-32px) px-4 md:px-0"
+          onSubmit={submitForm}
+        >
           <input
             className="bg-zinc-100 rounded-md px-2 py-1.5 flex-1 outline-none dark:bg-zinc-700 text-zinc-800 dark:text-zinc-300"
             placeholder="Send a message..."
@@ -167,7 +176,6 @@ export function Chat({
           ) : (
             <button
               className="relative text-sm bg-zinc-100 rounded-lg size-9 shrink-0 flex flex-row items-center justify-center cursor-pointer hover:bg-zinc-200 dark:text-zinc-50 dark:bg-zinc-700 dark:hover:bg-zinc-800"
-              onClick={submitForm}
               disabled={!input.trim()}
             >
               {status === "streaming" ? (
@@ -177,7 +185,7 @@ export function Chat({
               )}
             </button>
           )}
-        </div>
+        </form>
       </div>
     </div>
   );
